d8a2b0e081bf3584a4e29d8736e7621b9f3a9b6a
Alex <Alex@Laptop>
2014-02-01 19:13:43 -0800

    edited my.interface and classes
    question regarding setters w/ over-ridden methods

--- src/myinterface/EmployeeInterface.java
+++ src/myinterface/EmployeeInterface.java
@@ -12,8 +12,16 @@ package myinterface;
  */
 public interface EmployeeInterface {
     
+    public abstract String firstName(String firstName);
     
+    public abstract String lastName(String lastName);
     
+    public abstract String position(String position);
     
+    public abstract double biWeeklyEarnings();
+    
+    public abstract double monthlyEarnings();
+
+    public abstract double yearlyEarnings();
     
 }
--- src/myinterface/HourlyEmployeeInterface.java
+++ src/myinterface/HourlyEmployeeInterface.java
@@ -11,7 +11,66 @@ package myinterface;
  * @author Alex
  */
 public abstract class HourlyEmployeeInterface implements EmployeeInterface {
+
+    private String firstName, lastName, position;
+    private double hourlyPayRate;
+
     
-    public abstract void getBiMonthlyPay();
-    public abstract void getMonthlyPay();
+    //Setters
+                                                    
+    public void setFirstName(String firstName) {    //***************
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {      //***************
+        this.lastName = lastName;
+    }
+
+    public void setPosition(String position) {      //***************
+        this.position = position;
+    }
+    
+
+    public void setHourlyPayRate(double hourlyPayRate) {
+        this.hourlyPayRate = hourlyPayRate;
+    }  
+
+    public double getHourlyPayRate() {
+        return hourlyPayRate;
+    }
+    
+    //Over-ridden Methods
+    @Override                               //***************
+    public String firstName(String firstName){
+        this.firstName = firstName;
+                return firstName;
+    }
+    
+    @Override                               //***************
+    public String lastName(String lastName){
+        this.lastName = lastName;
+        return lastName;
+    }
+    
+    @Override                               //****************
+    public String position(String position){
+        this.position = position;
+        return position;
+    }
+    
+    @Override
+    public double biWeeklyEarnings(){
+        return hourlyPayRate * 80;
+    }
+    
+    @Override
+    public double monthlyEarnings(){
+        return hourlyPayRate * 160;
+    }
+    
+    @Override
+    public double yearlyEarnings(){
+        return ( hourlyPayRate * 160 ) * 12;
+    }
+
 }
